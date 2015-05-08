c337cd8106f7b833f4ee75ce36e5e77cca3f0449
Daniël de Kok <me@danieldk.eu>
2015-05-08 11:55:11 +0200

    Subpackage documentation 'headers'.

---
+++ features/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2015 The dpar Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package features provides feature generators for dependency parsers.
+package features
---
+++ system/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2015 The dpar Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package system provides various transition systems.
+//
+// This package implements the Arc-Standard, Arc-Eager, and
+// Stack-Projective systems. You can define your own transition
+// systems by implementing the Transition and TransitionSet
+// interfaces.
+package system
