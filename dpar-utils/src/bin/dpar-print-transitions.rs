3d85f3098d4bf25dac9a240f87bfd635f0872fb4
Daniël de Kok <me@danieldk.eu>
2018-07-30 16:57:03 +0200

    Add dpar-print-transitions.
    This is a small utility to extract a gold-standard transitions from a treebank.

---
+++ dpar-utils/src/bin/dpar-print-transitions.rs
@@ -0,0 +1,119 @@
+extern crate colored;
+extern crate conllx;
+extern crate dpar;
+#[macro_use]
+extern crate dpar_utils;
+extern crate getopts;
+extern crate stdinout;
+
+use std::env::args;
+use std::io::{BufRead, BufWriter, Write};
+use std::process;
+
+use colored::*;
+use conllx::{HeadProjectivizer, Projectivize, ReadSentence};
+use dpar::guide::Guide;
+use dpar::system::{sentence_to_dependencies, ParserState, Transition, TransitionSystem};
+use dpar::systems::{
+    ArcEagerSystem, ArcHybridSystem, ArcStandardSystem, StackProjectiveSystem, StackSwapSystem,
+};
+use getopts::Options;
+use stdinout::{Input, Output};
+
+use dpar_utils::{OrExit, Result};
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] SYSTEM [INPUT]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    let matches = opts.parse(&args[1..]).or_exit();
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    if matches.free.len() < 1 || matches.free.len() > 3 {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let input = Input::from(matches.free.get(1));
+    let reader = conllx::Reader::new(input.buf_read().or_exit());
+
+    let output = Output::from(matches.free.get(2));
+    let writer = BufWriter::new(output.write().or_exit());
+
+    parse(&matches.free[0], reader, writer).or_exit();
+}
+
+fn parse<R, W>(system: &str, reader: conllx::Reader<R>, writer: BufWriter<W>) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+{
+    match system {
+        "arceager" => parse_with_system::<R, W, ArcEagerSystem>(reader, writer),
+        "archybrid" => parse_with_system::<R, W, ArcHybridSystem>(reader, writer),
+        "arcstandard" => parse_with_system::<R, W, ArcStandardSystem>(reader, writer),
+        "stackproj" => parse_with_system::<R, W, StackProjectiveSystem>(reader, writer),
+        "stackswap" => parse_with_system::<R, W, StackSwapSystem>(reader, writer),
+        _ => {
+            stderr!("Unsupported transition system: {}", system);
+            process::exit(1);
+        }
+    }
+}
+
+fn parse_with_system<R, W, S>(reader: conllx::Reader<R>, mut writer: BufWriter<W>) -> Result<()>
+where
+    R: BufRead,
+    W: Write,
+    S: TransitionSystem,
+{
+    let projectivizer = HeadProjectivizer::new();
+
+    for sentence in reader.sentences() {
+        //let sentence = sentence;
+        let sentence = projectivizer.projectivize(&sentence?)?;
+
+        let gold_dependencies = sentence_to_dependencies(&sentence).or_exit();
+        let mut oracle = S::oracle(&gold_dependencies);
+
+        let mut state = ParserState::new(&sentence);
+        while !S::is_terminal(&state) {
+            let next_transition = oracle.best_transition(&state);
+            writeln!(writer, "{}", format!("{:?}", next_transition).purple())?;
+            writeln!(
+                writer,
+                "Stack: {}",
+                state
+                    .stack()
+                    .iter()
+                    .map(|&idx| state.tokens()[idx])
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )?;
+            writeln!(
+                writer,
+                "Buffer: {}",
+                state
+                    .buffer()
+                    .iter()
+                    .map(|&idx| state.tokens()[idx])
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )?;
+            next_transition.apply(&mut state);
+        }
+    }
+
+    Ok(())
+}
