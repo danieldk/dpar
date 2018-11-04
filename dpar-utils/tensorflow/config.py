204987ff537782984c5aeac60d5dc7e30034fbe1
Daniël de Kok <me@danieldk.eu>
2018-11-04 12:08:27 +0100

    Rewrite Python scripts.
    Since training is moved to rust, the scripts now only write the graph.
    The graph also expose the ops necessary for training/validation.

--- dpar-utils/tensorflow/config.py
+++ dpar-utils/tensorflow/config.py
@@ -1,12 +1,7 @@
 class DefaultConfig:
     init_scale = 0.05
-    batch_size = 8192
     hidden_size = 200
     morph_hidden_size = 50
     deprel_embed_size = 50
     keep_prob = 0.95
     keep_prob_input = 0.90
-    num_layers = 2
-    max_epoch = 30
-    lr_decay = 0.8
-    num_outputs = 0
